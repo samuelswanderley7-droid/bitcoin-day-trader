9e76a3d453ef6463b0a985053f7b6ee8064fca7d
samuelswanderley7-droid <samuel.swanderley7@gmail.com>
2026-02-21 13:08:11 +0100

    Create TradingViewModel.kt file

---
+++ app/src/main/java/com/bitcoindaytrader/ui/TradingViewModel.kt
@@ -0,0 +1,9 @@
+// TradingViewModel.kt
+
+package com.bitcoindaytrader.ui
+
+import androidx.lifecycle.ViewModel
+
+class TradingViewModel : ViewModel() {
+    // Add properties and methods for TradingViewModel
+}
